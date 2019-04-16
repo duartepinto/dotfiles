1e8a16ca08f10ccd63e97f4a8f2963695db806bc
Duarte Pinto <duartemr.pinto@gmail.com>
2019-04-16 17:55:41 +0100

    Update scripts to include ftplugin folders

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp .vimrc ~/
+mkdir -p ~/.vim/ftplugin/ && cp -R .vim/ftplugin/ "$_"
 cp .zshrc ~/
 cp .tmux.conf ~/
 cp .ideavimrc ~/
--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp ~/.vimrc .vimrc
+mkdir -p .vim/ftplugin/ && cp -R ~/.vim/ftplugin/ "$_"
 cp ~/.zshrc .zshrc
 cp ~/.tmux.conf .tmux.conf
 cp ~/.ideavimrc .ideavimrc
