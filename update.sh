bc5b6310319ecbbb5717a2d58ce9c0ed8da61639
Duarte Pinto <duartemr.pinto@gmail.com>
2019-05-20 11:13:30 +0100

    Add coc configs

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 cp ~/.vimrc .vimrc
 mkdir -p .vim/ftplugin/ && cp -R ~/.vim/ftplugin/ "$_"
+cp ~/.vim/coc-settings.json .vim/
 cp ~/.zshrc .zshrc
 cp ~/.tmux.conf .tmux.conf
 cp ~/.ideavimrc .ideavimrc
