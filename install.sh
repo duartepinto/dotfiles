6bf979db5c101a069b16e30426f0c5f29565dc4d
Duarte Pinto <duartemr.pinto@gmail.com>
2021-03-06 18:32:16 +0000

    Update install script to use stow instead of copying files

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-cp .vimrc ~/
-mkdir -p ~/.vim/ftplugin/ && cp -R .vim/ftplugin/ "$_"
-cp .zshrc ~/
-cp .tmux.conf ~/
-cp .ideavimrc ~/
-cp .gitignore_global ~/
+stow configs
--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp ~/.vimrc .vimrc
-mkdir -p .vim/ftplugin/ && cp -R ~/.vim/ftplugin/ "$_"
-cp ~/.vim/coc-settings.json .vim/
-cp ~/.zshrc .zshrc
-cp ~/.tmux.conf .tmux.conf
-cp ~/.ideavimrc .ideavimrc
-cp ~/.gitignore_global .gitignore_global
