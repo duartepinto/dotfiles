360ba2168aee55c82d54c16da972b4ca71d39c93
Duarte Pinto <duartemr.pinto@gmail.com>
2022-10-04 11:58:12 +0100

    Update nvim-metals keybindings

--- configs/.vim/vimrcs/nvim-metals.lua
+++ configs/.vim/vimrcs/nvim-metals.lua
@@ -41,22 +41,28 @@ vim.opt_global.shortmess:remove("F"):append("c")
 -- LSP mappings
 map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
 map("n", "gds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
 map("n", "gws", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
 map("n", "<leader>cl", [[<cmd>lua vim.lsp.codelens.run()<CR>]])
 map("n", "<leader>sh", [[<cmd>lua vim.lsp.buf.signature_help()<CR>]])
 map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 map("n", "<leader>F", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+map("n", "<leader>A", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = false })
 map("n", "Ks", '<cmd>lua require"metals".hover_worksheet()<CR>')
 map("n", "<leader>aa", [[<cmd>lua vim.diagnostic.setqflist()<CR>]]) -- all workspace diagnostics
 map("n", "<leader>ae", [[<cmd>lua vim.diagnostic.setqflist({severity = "E"})<CR>]]) -- all workspace errors
 map("n", "<leader>aw", [[<cmd>lua vim.diagnostic.setqflist({severity = "W"})<CR>]]) -- all workspace warnings
-map("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>") -- buffer diagnostics only
 map("n", "[g", "<cmd>lua vim.diagnostic.goto_prev { wrap = false }<CR>")
 map("n", "]g", "<cmd>lua vim.diagnostic.goto_next { wrap = false }<CR>")
+
+-- Defaults from nvim-metals suggested config
+-- map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+-- map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+-- map("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>")
+-- Overriden:
+map("n", "gi", '<cmd>lua require"telescope.builtin".lsp_implementations()<CR>')
+map("n", "gr", '<cmd>lua require"telescope.builtin".lsp_references()<CR>')
+map("n", "<leader>d", '<cmd>lua require"telescope.builtin".diagnostics()<CR>') -- buffer diagnostics only
 
 -- completion related settings
 -- This is similiar to what I use
