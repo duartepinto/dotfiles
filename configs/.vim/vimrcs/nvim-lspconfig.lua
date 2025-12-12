9e9115331739f3ad3af372d787476dc9c123eb52
Duarte Pinto <duartemr.pinto@gmail.com>
2025-12-12 15:17:15 +0000

    Add Lua LSP

--- configs/.vim/vimrcs/nvim-lspconfig.lua
+++ configs/.vim/vimrcs/nvim-lspconfig.lua
@@ -1,7 +1,29 @@
 vim.lsp.config('ts_ls', {
   on_attach = on_attach,
-  filetypes = {"typescript", "typescriptreact", "typescript.tsx", "javascript", "jsx"},
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "jsx" },
   cmd = { "typescript-language-server", "--stdio" },
 })
 
 vim.lsp.enable('pyright')
+
+vim.lsp.config('lua_ls', {
+  on_attach = on_attach,
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  -- Sets the "workspace" to the directory where any of these files is found.
+  -- Files that share a root directory will reuse the LSP server connection.
+  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  -- Specific settings to send to the server. The schema is server-defined.
+  -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT'
+      },
+      diagnostics = {
+        globals = { "vim" },
+      }
+    }
+  }
+})
